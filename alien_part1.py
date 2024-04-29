526d24b1e3679d8b1c6c6c4b638324a8cc29ca81
Elder Samuel Taveira da Silva <030699331694@tre-rn.jus.br>
2024-04-29 14:04:41 -0300

    chaging the color of the screen

--- alien_part1.py
+++ alien_part1.py
@@ -7,6 +7,8 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
     pygame.display.set_caption("Alien Invasion")
+    #Define a cor de fundo
+    bg_color = (230, 230, 230)
 
     # Start the main loop for the game
     while True:
@@ -15,6 +17,8 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        #Redesenha a tela a cada passagem pelo loop
+        screen.fill(bg_color)
         # Make the most recently drawn screen visible
         pygame.display.flip()
 
