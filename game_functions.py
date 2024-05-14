703548379c2988c955abc8c2d7060618d68d1c94
Elder Samuel Taveira da Silva <elder.silva.residente@tre-rn.jus.br>
2024-05-14 15:04:51 -0300

    REFACT: optimizing the check_events function and creating the keyup and keydown functions

--- game_functions.py
+++ game_functions.py
@@ -1,22 +1,30 @@
 import pygame
 import sys
 
+def check_keydown_events(event, ship):
+    """Responde ao pressionamento de teclas"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Responde a solturas de teclas"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """Responde aos eventos de teclado e mouse"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                # Move a espaçosave para a direita
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False 
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False 
+            check_keyup_events(event, ship)
+            
              
 
 def update_screen(ai_settings, screen, ship):
